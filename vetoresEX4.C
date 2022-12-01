c2f36bb0c899b2be8b6234d6245fd6d6e81a22a3
Luís H. Gonçalves de Oliveira <109926443+Luisinhoo@users.noreply.github.com>
2022-12-01 16:34:09 -0300

    Add files via upload
    Atividade LAB06 VETORES

---
+++ vetoresEX4.C
@@ -0,0 +1,49 @@
+/*
+   Atividade 4 de VETORES: Fazer um programa para ler 5 valores e, em seguida, mostrar a posição onde se encontram
+                           o maior e o menor valor.
+   POR LUÍS H.                        
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int valor[5];
+    int i;
+    int maior,menor,maiorPosicao,menorPosicao;
+
+    for(i = 0; i < 5; i++)
+    {
+            printf("Entre %dº numero: ", i+1);
+            scanf("%d", &valor[i]);
+
+            if(i == 0)
+            {
+                maior = valor[i];
+                menor = valor[i];
+                maiorPosicao = i;
+                menorPosicao = i;
+            }
+
+            else if(valor[i] > maior)
+            {
+                maior = valor[i];
+                maiorPosicao = i;
+            }
+            else if(valor[i] < menor)
+            {
+                menor = valor[i];
+                menorPosicao = i;
+            }
+
+    }
+    printf("Maior valor: %d\n", maior);
+    printf("Posicao [%d]\n", maiorPosicao);
+
+    printf("\n");
+
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao [%d]\n", menorPosicao);
+
+    return 0;
+}
